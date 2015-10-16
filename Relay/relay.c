37b06a1785eb44d4af2967b777771cee310d39d2
Craig Haywood <craig.haywood@protonmail.com>
2015-10-16 11:58:11 +1000

    Implemented infrastructure for handling and threading client connections. Thread management yet to be completed, however at this stage new thread is being created and its metadata stored upon client connection

--- Relay/relay.c
+++ Relay/relay.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "relay.h"
 
 #define ENABLE_LOGGING
+
+client_thread_description cthread_pool[NUM_CLIENT_HANDLER_THREADS];
+client_thread_description *first_ct = NULL, *last_ct = NULL;
+unsigned int num_active_client_threads = 0;
 
 int main(int argc, char const *argv[])
 {
@@ -16,7 +24,7 @@ int main(int argc, char const *argv[])
 	int ret, listening_socket, client_socket;
 	int errno_cached;
 	socklen_t sockaddr_len;
-	struct sockaddr client_addr;
+	struct sockaddr_in client_addr;
 
 	if(argc != 2) {
 		fprintf(stdout, "Usage: ./%s [PORT]\n", program_name);
@@ -27,10 +35,19 @@ int main(int argc, char const *argv[])
 		fprintf(stdout, "%s program begin\n", program_name);
 	#endif
 
+	ret = initialize_ct_thread_pool(cthread_pool, (sizeof(cthread_pool)/sizeof(client_thread_description)));
+	if(ret < 0) {
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "Failed to initialize program memory");
+		#endif
+
+		exit(-1);
+	}
+
 	port = (unsigned int)atoi(argv[1]);
 	if(port > PORT_MAX) {
 		fprintf(stdout, "Port number (%u) must be less than %u\n", port, PORT_MAX);
-		exit(-1);
+		exit(-2);
 	}
 
 	ret = init_listening_socket(port, &listening_socket);
@@ -39,7 +56,7 @@ int main(int argc, char const *argv[])
 			fprintf(stdout, "Failed to initialize listening socket on port %u\n", port);
 		#endif
 
-		exit(-2);
+		exit(-3);
 	}
 
 	#ifdef ENABLE_LOGGING
@@ -49,7 +66,7 @@ int main(int argc, char const *argv[])
 	while(1) {
 		sockaddr_len = sizeof(client_addr);
 		bzero((char *) &client_addr, sizeof(client_addr));
-		client_socket = accept(listening_socket, &client_addr, &sockaddr_len);
+		client_socket = accept(listening_socket, (struct sockaddr *)&client_addr, &sockaddr_len);
 		if(client_socket < 0) {
 			errno_cached = errno;
 			#ifdef ENABLE_LOGGING
@@ -58,11 +75,148 @@ int main(int argc, char const *argv[])
 
 			continue;
 		}
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "%s:%d connected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		#endif
 
-
+		handle_new_client_connection(client_socket);		
 	}
 
 	return 0;
+}
+
+int handle_new_client_connection(int client_socket)
+{
+	int ret, errno_cached;
+	int unused_thread_index;
+
+	if(client_socket < 0) {
+		return -1;
+	}
+
+	if(num_active_client_threads >= NUM_CLIENT_HANDLER_THREADS) {
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "Unable to accept new client socket connections as number of connections (%u) has reached maximum allowed\n", num_active_client_threads);
+		#endif
+
+		close(client_socket);
+		return -1;
+	}
+
+	get_index_of_unused_thread_descriptor(cthread_pool, (sizeof(cthread_pool)/sizeof(client_thread_description)), &unused_thread_index);
+	if(unused_thread_index == -1) {
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "Client thread pool reached maximum, rejecting client connection attempt");
+		#endif
+
+		close(client_socket);
+		return -1;
+	}
+
+	ret = pthread_create(&cthread_pool[unused_thread_index].thread_id, NULL, handle_client_thread, &client_socket);
+	if(ret != 0) {
+		errno_cached = errno;
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "Failed to create client handler thread, %s\n", strerror(errno_cached));
+		#endif
+
+		close(client_socket);
+		return -1;
+	}
+	cthread_pool[unused_thread_index].thread_age = 0;
+	cthread_pool[unused_thread_index].next = NULL;
+	if(first_ct == NULL) {
+		first_ct = &cthread_pool[unused_thread_index];
+		last_ct = first_ct;
+	} else {
+		first_ct->next = &cthread_pool[unused_thread_index];
+		first_ct = first_ct->next;
+	}
+	num_active_client_threads++;
+
+	return 0;
+}
+
+int get_index_of_unused_thread_descriptor(client_thread_description *cthread_pool, unsigned int thread_pool_length, int *index /* out */)
+{
+	unsigned int i;
+
+	if(cthread_pool == NULL) {
+		return -1;
+	}
+	if(index == NULL) {
+		return -1;
+	}
+
+	*index = -1;
+	for (i = 0; i < thread_pool_length; ++i) {
+		if(cthread_pool[i].thread_age == -1) {
+			*index = i;
+		}
+	}
+
+	return 0;
+}
+
+int initialize_ct_thread_pool(client_thread_description *cthread_pool, unsigned int thread_pool_length)
+{
+	unsigned int i;
+
+	if(cthread_pool == NULL) {
+		return -1;
+	}
+
+	for (i = 0; i < thread_pool_length; ++i) {
+		cthread_pool[i].thread_age = -1;
+		cthread_pool[i].next = NULL;
+	}
+
+	return 0;
+}
+
+void *handle_client_thread(void *ptr)
+{
+	int client_socket;
+	char *pthread_ret;
+
+	if(ptr == NULL) {
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "Handle client thread created with null arguments\n");
+		#endif
+
+		pthread_ret = (char *)-1;
+		pthread_exit(pthread_ret);
+	}
+	#ifdef ENABLE_LOGGING
+		fprintf(stdout, "Created new client thread\n");
+	#endif
+	client_socket = *((int *)ptr);
+
+	sleep(3);
+
+	#ifdef ENABLE_LOGGING
+		fprintf(stdout, "Client thread exit\n");
+	#endif
+	close(client_socket);
+
+	pthread_ret = (char *)0;
+	pthread_exit(pthread_ret);
+}
+
+void *manage_clients_threads_thread(void *ptr)
+{
+	char *pthread_ret;
+
+	#ifdef ENABLE_LOGGING
+		fprintf(stdout, "Created manage client thread\n");
+	#endif
+
+	while(1) {
+		sleep(10);
+	}
+
+	pthread_ret = (char *)0;
+	pthread_exit(pthread_ret);
 }
 
 int init_listening_socket(unsigned int port, int *listening_socket /* out */)
--- Relay/relay.h
+++ Relay/relay.h
@@ -3,10 +3,24 @@
 
 char *program_name = "Relay";
 
-#define PORT_MAX 			(65535)
+#define PORT_MAX 						(65535)
+#define LISTEN_BACKLOG_MAX 				(50)
 
-#define LISTEN_BACKLOG_MAX 	(50)
+#define NUM_CLIENT_HANDLER_THREADS 		(500)
+#define CLIENT_HANDLER_THREAD_MAX_AGE 	(10)
+
+typedef struct client_thread_description
+{
+	pthread_t thread_id;
+	int thread_age;
+	struct client_thread_description *next;
+} client_thread_description;
 
 int init_listening_socket(unsigned int port, int *listening_socket /* out */);
+int handle_new_client_connection(int client_socket);
+void *handle_client_thread(void *ptr);
+void *manage_clients_threads_thread(void *ptr);
+int initialize_ct_thread_pool(client_thread_description *cthread_pool, unsigned int thread_pool_length);
+int get_index_of_unused_thread_descriptor(client_thread_description *cthread_pool, unsigned int thread_pool_length, int *index /* out */);
 
 #endif
