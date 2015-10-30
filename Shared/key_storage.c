938dc46eb37642db28b37b50393d27f653875fe7
Craig Haywood <craig.haywood@protonmail.com>
2015-10-30 11:05:09 +1000

    Added function which returns amount of free RAM

--- Shared/key_storage.c
+++ Shared/key_storage.c
@@ -1,10 +1,32 @@
 #include "key_storage.h"
 
 #define ENABLE_LOGGING
+#define DEBUG_MODE
 
 key *key_store = NULL;
 
 unsigned int max_user_id, key_storage_size;
+
+#ifdef DEBUG_MODE
+
+int main(int argc, char const *argv[])
+{
+	unsigned long mem_free_bytes;
+	int ret;
+
+	fprintf(stdout, "DEBUG_MODE Begin\n");
+
+	ret = get_free_mem_byte(&mem_free_bytes);
+	if(ret < 0) {
+		fprintf(stdout, "Failed to get free memory\n");
+
+		return -1;
+	}
+	
+	return 0;
+}
+
+#endif
 
 int init_key_store(char *thread_id)
 {
@@ -151,4 +173,112 @@ int get_max_user_id(void)
 	init_key_store("[MAIN THREAD]");
 
 	return max_user_id;
+}
+
+int get_free_mem_byte(unsigned long *mem_free_bytes)
+{
+	int ret, i, j, k;
+	FILE *fp;
+	char line[256], *c_ptr;
+	const char *memfree_str = "memfree";
+	char free_bytes_buf[32], qualifier_buf[3];
+	int found_free_bytes, prev_char_was_digit, found_qualifier;
+	float multiplier;
+
+	if(mem_free_bytes == NULL) {
+		return -1;
+	}
+
+	fp = fopen(MEM_STAT_FILE, "r");
+	if(fp == NULL) {
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "Failed to open memory statistics file, %s\n", MEM_STAT_FILE);
+		#endif
+
+		return -1;
+	}
+
+	while(1) {
+		c_ptr = fgets(line, sizeof(line), fp);
+		if(c_ptr == NULL) {
+			fclose(fp);
+			return -1;
+		}
+		ret = strncasecmp(memfree_str, (const char *)line, (sizeof(memfree_str) - 1));
+		if(ret == 0) {
+			break;
+		}
+	}
+
+	memset(free_bytes_buf, 0, sizeof(free_bytes_buf));
+	memset(qualifier_buf, 0, sizeof(qualifier_buf));
+	found_free_bytes = prev_char_was_digit = 0;
+	found_qualifier = 0;
+	j = k = 0;
+	for(i = 0; i < 256; i++) {
+		if(line[i] == '\0')
+			break;
+		if(line[i] == '\n')
+			break;
+
+		if((found_free_bytes == 0) || (prev_char_was_digit == 1)) {
+			if(isdigit(line[i]) != 0) {
+				if(j >= (sizeof(free_bytes_buf) - 1)) {
+					fclose(fp);
+					return -1;
+				}
+				free_bytes_buf[j++] = line[i];
+				found_free_bytes = 1;
+				prev_char_was_digit = 1;
+			} else {
+				prev_char_was_digit = 0;
+			}
+		} else {
+			if(found_free_bytes == 1) {
+				if(isalpha(line[i]) != 0) {
+					if(k >= (sizeof(qualifier_buf) - 1)) {
+						fclose(fp);
+						return -1;
+					}
+					qualifier_buf[k++] = line[i];
+					found_qualifier = 1;
+				}
+			}
+		}
+	}
+
+	if(found_free_bytes == 1) {
+		switch(qualifier_buf[0]) {
+			case 'k':
+			case 'K':
+				multiplier = (1.0/1024.0);
+			break;
+			case 'm':
+			case 'M':
+				multiplier = 1.0;
+			break;
+			case 'g':
+			case 'G':
+				multiplier = (1024.0);
+			break;
+			default:
+				multiplier = (1.0/1024.0);
+			break;
+		}
+		*mem_free_bytes = (unsigned long)(multiplier * (float)atol(free_bytes_buf));
+
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "Found memory statistics: %lu MB\n", *mem_free_bytes);
+		#endif
+
+		fclose(fp);
+		return 0;
+	} else {
+		#ifdef ENABLE_LOGGING
+			fprintf(stdout, "Failed to parse memory statistics, found_free_bytes: %d, found_qualifier: %d\n", found_free_bytes, found_qualifier);
+		#endif
+
+		fclose(fp);
+		return -1;
+	}
 }
--- Shared/key_storage.h
+++ Shared/key_storage.h
@@ -6,12 +6,15 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 
 #define USER_ID_BITS 			(20) // Raspberry PI 1
 //#define USER_ID_BITS 			(24) // Raspberry PI 2
 
 #define AES_KEY_SIZE_BITS 		(128)
 #define AES_KEY_SIZE_BYTES 		(AES_KEY_SIZE_BITS/8)
+
+#define MEM_STAT_FILE 			("/proc/meminfo")
 
 typedef struct key
 {
@@ -25,5 +28,6 @@ int remove_key_from_key_store(char *thread_id, unsigned int user_id);
 int set_key_for_user_id(char *thread_id, unsigned int user_id, key *key_in);
 int get_key_for_user_id(char *thread_id, unsigned int user_id, key *key_out /* out */);
 int get_max_user_id(void);
+int get_free_mem_byte(unsigned long *mem_free_bytes);
 
 #endif
